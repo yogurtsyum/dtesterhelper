3659dbe6c52bd36c5848200953455fea4320d8f5
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2020-07-13 14:45:57 -0500

    Update ios.js

--- commands/search/ios.js
+++ commands/search/ios.js
@@ -107,6 +107,6 @@ module.exports = class iOSCommand extends Command {
             awaitreact();
           }).catch(console.error);
         }
-        awaitreact();
+        return awaitreact();
     }
 }
